903fd1b21ab7a5640d85927a64acedccc31c1261
Craig Burke <cragardev@gmail.com>
2022-03-11 07:02:16 -0600

    initial commit

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def checker8():
+    return render_template("index.html")
+
+
+@app.route('/<int:num>')
+def checker4(num):
+    return render_template("index.html", num=num)
+
+
+@app.route('/<int:num1>/<int:num2>')
+def checker_x_by_y(num1, num2):
+    return render_template("index2.html", num1=num1, num2=num2)
+
+
+@app.route('/<int:num1>/<int:num2>/<color1>/<color2>')
+def checker_x_by_y_color_by_color(num1, num2, color1, color2):
+    return render_template("index.html", num1=num1, num2=num2, color1=color1, color2=color2)
+
+
+#! MUST BE AT THE BOTTOM ---------------
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+console.log();
+console.log("Hello Craig, How are we today?");
+console.log();
+// ? -- starting a js file
+//
+
+console.log();
+console.log("end of file");
+console.log();
